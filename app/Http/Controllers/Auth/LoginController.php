169193ab092886737df56f3657a04c4278da3995
MarioQuiroga <marioquiro17@gmail.com>
2018-04-08 16:54:18 -0300

    elimio readme

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+//Usos incluidos en el tutorial
+use Validator;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\RegisterUsers;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\Request;
+use Session;
+
 class LoginController extends Controller
 {
     /*
@@ -32,8 +41,59 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct(Guard $auth){
+        $this->auth = $auth;
         $this->middleware('guest')->except('logout');
     }
+
+// Login
+    protected function getLogin(){
+        return view('login');
+    
+    }
+// Procesar los datos enviados en el login
+    protected function postLogin(Request $request){
+            return Validator::make($request, [
+                'name' => 'required',
+                'password' => 'required',
+            ]);
+            
+        $credenciales = $request->only('name', 'password');
+
+        if ($this->auth->attempt($credenciales, $request->has('remember'))){
+            return view($redirectTo);
+        }
+        return view(login);
+    }
+
+// Registrarse en el sistema
+    protected function getRegister(){
+        return view('registro');    
+    }
+
+    protected function postRegister(Request $request){
+        return Validator::make($request, [
+                'name' => 'required',
+                'email'=>'required',
+                'password' => 'required',
+            ]);
+        $data = $request;
+
+        $user = new User;
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        if($user->save()){
+            return "Se ha registrado correctamente el usuario";
+        }
+
+    }
+// LogOut
+    protected function getLogout(){
+        $this->auth->logout();
+        Session::flush();
+        return redirect('login');
+    }    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
+/*
 Route::get('login ', function () {
     return view('login');
 });
@@ -22,3 +22,16 @@ Route::get('login ', function () {
 Route::get('index', function () {
     return view('index');
   });
+
+  */
+  Route::get('login', 'Auth\LoginController@getLogin');
+  Route::post('login', ['as'=>'login', 'uses'=>'Auth\LoginController@postLogin']);
+  Route::get('logout', ['as'=>'logout', 'uses'=>'Auth\LoginController@getLogout']);
+
+  //Registro de rutas
+ Route::get('register', 'Auth\RegisterController@getRegister');
+ Route::post('register', ['as'=>'auth\register', 'uses'=>'Auth\RegisterController@postRegister']);
+
+ Route::get('/', 'HomeController@index');
+ Route::get('home', 'HomeController@index');
+ Route::get('index', 'HomeController@index');
